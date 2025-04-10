e1501bcbdbfc30ad661998db63b5eff42fc667d2
Jan Jablunka <jablunka@gmail.com>
2025-04-10 20:37:47 +0200

    Update PostBlockFileCollection and index.twig template

--- app/Classes/Posts/PostBlockFileCollection.php
+++ app/Classes/Posts/PostBlockFileCollection.php
@@ -7,10 +7,20 @@ use Katu\Files\UploadCollection;
 
 class PostBlockFileCollection extends \ArrayObject
 {
-	public static function createFromUploads(PostBlock $postBlock, UploadCollection $uploads)
+	public static function createFromUploads(PostBlock $postBlock, UploadCollection $uploads): PostBlockFileCollection
 	{
 		return new PostBlockFileCollection(array_map(function (Upload $upload) use ($postBlock) {
 			return PostBlockFile::createFromUpload($postBlock, $upload);
 		}, $uploads->getArrayCopy()));
+	}
+
+	public function getSlice(int $length): PostBlockFileCollection
+	{
+		return new PostBlockFileCollection(array_slice($this->getArrayCopy(), 0, $length));
+	}
+
+	public function getFirst(): ?PostBlockFile
+	{
+		return array_values($this->getArrayCopy())[0] ?? null;
 	}
 }
